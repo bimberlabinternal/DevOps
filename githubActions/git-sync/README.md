a8e85cd10172ba67ee4be78033e85f4f2ff58bba
bbimber <bbimber@gmail.com>
2020-02-01 07:15:49 -0800

    Initial git-sync action

---
+++ githubActions/git-sync/README.md
@@ -0,0 +1,3 @@
+# git-sync
+
+Sync a given branch with a remote using git merge --ff-only
---
+++ githubActions/git-sync/git-sync.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -e
+
+SOURCE_REPO=$1
+SOURCE_BRANCH=$2
+DESTINATION_BRANCH=$3
+DESTINATION_REPO=$4
+
+if [ -z $DESTINATION_REPO ];
+	DESTINATION_REPO = $GITHUB_REPOSITORY
+fi
+
+if ! echo $SOURCE_REPO | grep '.git'
+then
+  if [[ -n "$SSH_PRIVATE_KEY" ]]
+  then
+    SOURCE_REPO="git@github.com:${SOURCE_REPO}.git"
+    GIT_SSH_COMMAND="ssh -v"
+  else
+    SOURCE_REPO="https://github.com/${SOURCE_REPO}.git"
+  fi
+fi
+if ! echo $DESTINATION_REPO | grep '.git'
+then
+  if [[ -n "$SSH_PRIVATE_KEY" ]]
+  then
+    DESTINATION_REPO="git@github.com:${DESTINATION_REPO}.git"
+    GIT_SSH_COMMAND="ssh -v"
+  else
+    DESTINATION_REPO="https://github.com/${DESTINATION_REPO}.git"
+  fi
+fi
+
+echo "SOURCE=$SOURCE_REPO:$SOURCE_BRANCH"
+echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
+
+git clone "$DESTINATION_REPO" --origin DESTINATION && cd `basename "$DESTINATION_REPO" .git`
+git remote add source "$SOURCE_REPO"
+
+git checkout ${DESTINATION_BRANCH}
+git fetch source
+
+#git merge --ff-only source/${SOURCE_BRANCH}
+#git push
