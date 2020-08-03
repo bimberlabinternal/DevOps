5131fa76e21502c9548d6f0429cec49bff586c81
bbimber <bbimber@gmail.com>
2020-08-03 16:08:27 -0700

    Check prime-seq XML files into git

--- servers/mgap/installLabkey.sh
+++ servers/mgap/installLabkey.sh
@@ -27,3 +27,13 @@ bash $INSTALL $SETTINGS
 
 rm $INSTALL
 rm $SETTINGS
+
+# Only populate from github when missing.  This allows one-off local edits
+CONFIG_DIR=${LABKEY_HOME}/configs
+if [ ! -e $CONFIG_DIR ];then
+	mkdir -p $CONFIG_DIR
+	
+	wget -O ${CONFIG_DIR}/sequenceanalysisConfig.xml https://github.com/bimberlabinternal/DevOps/raw/master/servers/mgap/configs/sequenceanalysisConfig.xml
+	wget -O ${CONFIG_DIR}/pipelineConfig.xml https://github.com/bimberlabinternal/DevOps/raw/master/servers/mgap/configs/pipelineConfig.xml
+	wget -O ${CONFIG_DIR}/blastConfig.xml https://github.com/bimberlabinternal/DevOps/raw/master/servers/mgap/configs/blastConfig.xml
+fi
