40d465012e0e5e60f97b2185c41d3a3ef83f8ffa
bbimber <bbimber@gmail.com>
2020-08-03 15:53:40 -0700

    Use src dir as LK home on exacloud

--- servers/exacloud/exacloudSettings.sh
+++ servers/exacloud/exacloudSettings.sh
@@ -8,12 +8,12 @@ MAJOR=20
 MINOR_FULL="7"
 MINOR_SHORT=7
 
-LABKEY_HOME=/home/exacloud/lustre1/prime-seq/labkey
+LABKEY_HOME=/home/exacloud/lustre1/prime-seq/src
 LABKEY_USER=labkey_submit
 
 JAVA_HOME=/home/groups/prime-seq/exacloud/java/current
 JAVA=${JAVA_HOME}/bin/java
-LK_SRC_DIR=/home/exacloud/lustre1/prime-seq/src
+LK_SRC_DIR=${LABKEY_HOME}
 TOMCAT_HOME=/home/exacloud/lustre1/prime-seq/tomcat
 
 SKIP_INSTALL=1
