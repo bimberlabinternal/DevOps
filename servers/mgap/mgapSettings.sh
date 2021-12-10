dc470277aac2bf489987c5b34f0a1ef2711f28a0
bbimber <bbimber@gmail.com>
2021-12-09 16:21:51 -0800

    Update mgap and mcc to 21.11

--- servers/mgap/mgapSettings.sh
+++ servers/mgap/mgapSettings.sh
@@ -5,8 +5,8 @@ set -e
 #mGAP:
 
 MAJOR=21
-MINOR_FULL="3"
-MINOR_SHORT=3
+MINOR_FULL="11"
+MINOR_SHORT=$MINOR_FULL
 
 LKENV=$(grep ^EnvironmentFile /etc/systemd/system/labkey.service | cut -d = -f2 | sed 's/ //g')
 TOMCAT_HOME=$(grep ^CATALINA_HOME $LKENV | cut -d= -f2 | sed 's/ //g')
