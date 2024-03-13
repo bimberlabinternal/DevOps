66061e8bb234502a8fc05c740a2f7df96cb8120c
bbimber <bbimber@gmail.com>
2024-03-12 19:16:29 -0700

    Bugfix

--- scripts/downloadNovogene.sh
+++ scripts/downloadNovogene.sh
@@ -23,18 +23,18 @@ fi
 
 cd $DIRNAME
 
-SCRIPT=${1}.sh
 PWD=`pwd`
 
-wget -O $SCRIPT https://raw.githubusercontent.com/bimberlabinternal/DevOps/master/scripts/downloadNovogeneScript.sh
+#SCRIPT=${1}.sh
+#wget -O $SCRIPT https://raw.githubusercontent.com/bimberlabinternal/DevOps/master/scripts/downloadNovogeneScript.sh
 
 sbatch \
 	--job-name=$1 \
 	--mem 8000 \
-	--cpus-per-task=8
+	--cpus-per-task=8 \
 	--output=${1}.log \
 	--error=${1}.log \
 	--partition=exacloud \
 	--time=0-36 \
-	--chdir=$PWD
+	--chdir=$PWD \
 	~/bin/downloadNovogeneScript.sh $1 $2 $3
