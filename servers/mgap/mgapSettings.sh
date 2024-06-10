e3e682bc24a9d807cebcd6264bed2d91ebe2d4d8
bbimber <bbimber@gmail.com>
2024-06-09 22:14:02 -0700

    Update mgapSettings.sh

--- servers/mgap/mgapSettings.sh
+++ servers/mgap/mgapSettings.sh
@@ -8,8 +8,7 @@ MAJOR=24
 MINOR_FULL="3"
 MINOR_SHORT=$MINOR_FULL
 
-LKENV=$(grep ^EnvironmentFile /etc/systemd/system/labkey.service | cut -d = -f2 | sed 's/ //g')
-TOMCAT_HOME=$(grep ^CATALINA_HOME $LKENV | cut -d= -f2 | sed 's/ //g')
+TOMCAT_HOME=$(grep CATALINA_BASE /etc/systemd/system/labkey_server.service | cut -d = -f3 | sed 's/"//g')
 
 LABKEY_HOME=/usr/local/labkey
 LABKEY_USER=mgaplabkey
