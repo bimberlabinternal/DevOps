02b7ce88719ff7bf68b7696a09869eceb5da4e1e
bbimber <bbimber@gmail.com>
2024-07-15 13:12:07 -0700

    Server config updates

--- servers/prime-seq/initialSetup.sh
+++ servers/prime-seq/initialSetup.sh
@@ -9,6 +9,15 @@ wget -O /etc/monitrc https://raw.githubusercontent.com/bimberlabinternal/DevOps/
 wget -O /etc/monit.d/server https://raw.githubusercontent.com/bimberlabinternal/DevOps/master/servers/prime-seq/monit/monit.d/server
 service monit restart
 
+# Java:
+cd /usr/local/src
+wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.11%2B9/OpenJDK17U-jdk_x64_linux_hotspot_17.0.11_9.tar.gz
+tar -xf OpenJDK17U-jdk_x64_linux_hotspot_17.0.11_9.tar.gz
+mv jdk-17.0.11+9 ../
+rm -Rf OpenJDK17U-jdk_x64_linux_hotspot_17.0.11_9.tar.gz
+cd ../
+ln -s jdk-17.0.11+9 java
+
 # ActiveMQ:
 cd /usr/local/src
 wget https://dlcdn.apache.org//activemq/5.18.4/apache-activemq-5.18.4-bin.tar.gz
@@ -23,4 +32,18 @@ chown -R activemq:activemq ./apache-activemq-5.18.4
 # Services:
 wget -O /etc/systemd/system/labkey_server.service https://raw.githubusercontent.com/bimberlabinternal/DevOps/master/servers/prime-seq/labkey_server.service
 wget -O /etc/systemd/system/activemq.service https://raw.githubusercontent.com/bimberlabinternal/DevOps/master/servers/prime-seq/activemq.service
+systemctl daemon-reload
 
+if [ ! -e /usr/local/tools/ ]; then
+	mkdir /usr/local/tools
+fi
+
+wget -O /usr/local/tools/labkey-error-email.sh https://raw.githubusercontent.com/bimberlabinternal/DevOps/master/scripts/labkey-error-email.sh
+chmod +x /usr/local/tools/labkey-error-email.sh
+
+# LabKey
+cd /usr/local/src
+wget https://raw.githubusercontent.com/bimberlabinternal/DevOps/master/servers/prime-seq/installLabkey.sh
+chmod +x installLabkey.sh
+./installLabkey.sh
+chown -R labkey:labkey /usr/local/labkey/
