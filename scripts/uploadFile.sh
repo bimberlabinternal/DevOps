6adbee66368e2e7c7d04201e59552a09c8707180
bbimber <bbimber@gmail.com>
2024-10-01 10:04:20 -0700

    Add auto-retry to SRA download

--- scripts/uploadFile.sh
+++ scripts/uploadFile.sh
@@ -16,9 +16,32 @@ ASPERA_SCP_PASS=$KEY_FILE
 export PATH=~/.aspera/connect/bin/:$PATH
 echo 'Starting file: '$FILE
 
+function retry {
+	command="$*"
+	retval=1
+	attempt=1
+	until [[ $retval -eq 0 ]] || [[ $attempt -gt 5 ]]; do
+		# Execute inside of a subshell in case parent
+		# script is running with "set -e"
+		(
+			set +e
+			$command
+		)
+		retval=$?
+		attempt=$(( $attempt + 1 ))
+		if [[ $retval -ne 0 ]]; then
+			# If there was an error wait 10 seconds
+			sleep 10
+		fi
+	done
+	if [[ $retval -ne 0 ]] && [[ $attempt -gt 5 ]]; then
+		exit $retval
+	fi
+}
+
 if [ ! -e $DONE_FILE ];then
 	#NOTE: -k0 can be used to force re-upload. k1 allows resume
-	$ASCP -i $KEY_FILE -QT -l300m -k0 -d $FILE subasp@upload.ncbi.nlm.nih.gov:uploads/bbimber@gmail.com_oceqPdPd/$SUBMISSION/
+	retry $ASCP -i $KEY_FILE -QT -l300m -k0 -d $FILE subasp@upload.ncbi.nlm.nih.gov:uploads/bbimber@gmail.com_oceqPdPd/$SUBMISSION/
 	touch $DONE_FILE
 else
 	echo 'Transfer already complete'
