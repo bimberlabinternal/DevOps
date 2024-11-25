bb66736ea841b7e9670f9545e1596a48815d7e20
bbimber <bbimber@gmail.com>
2024-11-25 10:48:19 -0800

    Update installLabkey.sh

--- servers/prime-seq/installLabkey.sh
+++ servers/prime-seq/installLabkey.sh
@@ -29,7 +29,7 @@ if [ ! -e $LK_CONFIG ];then
 	chmod +x $LK_CONFIG/labkeyServerStartup.sh
 fi
 
-CONFIG_DIR=${LABKEY_HOME}/config
+CONFIG_DIR=/usr/local/labkey/config
 if [ ! -e $CONFIG_DIR ];then
 	mkdir -p $CONFIG_DIR
 	
