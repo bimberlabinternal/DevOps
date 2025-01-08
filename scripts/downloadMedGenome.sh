f60345d5477dda0e4a6102866cf8d00e4556264a
bbimber <bbimber@gmail.com>
2025-01-07 17:58:05 -0800

    Add download script for MedGenome

---
+++ scripts/downloadMedGenome.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z $1 ]];then
+	echo 'Must provide MedGenome project'
+ 	exit 1
+fi
+
+if [[ -z $2 ]];then
+	echo 'Must provide password'
+ 	exit 1
+fi
+
+if [[ -z $3 ]];then
+	echo 'Must provide expt'
+ 	exit 1
+fi
+
+DIRNAME=$1
+if [ ! -e $DIRNAME ];then
+	mkdir $DIRNAME
+fi
+
+cd $DIRNAME
+
+PWD=`pwd`
+
+SCRIPT=${1}.sh
+wget -O $SCRIPT https://raw.githubusercontent.com/bimberlabinternal/DevOps/master/scripts/downloadMedGenomeScript.sh
+chmod +x $SCRIPT
+
+sbatch \
+	--job-name=$1 \
+	--mem 8000 \
+	--cpus-per-task=8 \
+	--output=${1}.log \
+	--error=${1}.log \
+	--partition=batch \
+	--time=0-36 \
+	--chdir=$PWD \
+	./$SCRIPT $1 $2 $3
---
+++ scripts/downloadMedGenomeScript.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+DIRNAME=$1
+PW=$2
+
+if [[ -z $1 ]];then
+	echo 'Must provide MedGenome project'
+ 	exit 1
+fi
+
+if [[ -z $2 ]];then
+	echo 'Must provide password'
+ 	exit 1
+fi
+
+if [[ ! -z $3 ]];then
+	EXPT=$3
+	echo "Expt: "$EXPT
+fi
+
+if [ ! -e $DIRNAME ];then
+	mkdir $DIRNAME
+fi
+
+cd $DIRNAME
+
+HOST=portal-us.medgenome.com
+PORT=2424
+wget -nv -r -c --password=${PW} --reject-regex="_I1_|_I2_|Undetermined_" ftp://${DIRNAME}@${HOST}:${PORT}/FASTQ
+touch download.${DIRNAME}.done
+find . -name '*.gz' -exec mv {} ./ \;
+
+if ls Undetermined_* 1> /dev/null 2>&1; then
+	rm Undetermined_*
+fi
+
+if ls *_I1_* 1> /dev/null 2>&1; then
+	rm *_I1_*
+fi
+
+if ls *_I2_* 1> /dev/null 2>&1; then
+	rm *_I2_*
+fi
+
+echo 'File size:'
+du -s -h ../$DIRNAME
+
+if [[ ! -z $3 ]];then
+	echo "Copying data"
+	cd ../
+	mv $DIRNAME /home/groups/BimberLab/primeseq/${EXPT}/@files/
+	touch /home/groups/BimberLab/primeseq/${EXPT}/@files/${DIRNAME}/move.${DIRNAME}.done
+else
+	echo "Experiment not provided, will not move"
+fi
+
+echo 'done: '$DIRNAME
